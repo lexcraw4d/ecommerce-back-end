5b2d716d9248fa1d838f7485ce5efd2b1ca6a253
Lexie Crawford <lexcraw4d@gmail.com>
2021-06-21 01:44:32 -0400

    all routes work for CRUD - cleaned up code to make clear for developer using DRY and comments limited, but concise

--- models/index.js
+++ models/index.js
@@ -6,32 +6,32 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+	foreignKey: 'category_id',
+	onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+	foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'product_tags',
-  foreignKey: 'product_id',
+	through: ProductTag,
+	// as: 'product_tags',
+	foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: 'product_tags',
-  foreignKey: 'tag_id',
+	through: ProductTag,
+	// as: 'product_tags',
+	foreignKey: 'tag_id',
 });
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+	Product,
+	Category,
+	Tag,
+	ProductTag,
 };
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,106 +2,50 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-router.get('/', (req, res) => {
-	// find all categories
-	Category.findAll({
-		attributes: ['id', 'category_name'],
-		include: [
-			{
-				model: Product,
-				//if wanted to return single or select certain individual products you would list here in attributes
-				// attributes: ['product_name'],
-			},
-		],
-	})
-		.then((dbCategoryData) => res.json(dbCategoryData))
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
 
-	// be sure to include its associated Products
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: [Product],
+  })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
-	// find one category by its `id` value
-	router.get('/:id', (req, res) => {
-		Category.findOne({
-			where: {
-				id: req.params.id,
-			},
-			include: [
-				{
-					model: Product,
-					// attributes: ['product_name'],
-				},
-			],
-		})
-			.then((dbCategoryData) => {
-				if (!dbCategoryData) {
-					res.status(404).json({ message: 'No post found with this id' });
-					return;
-				}
-				res.json(dbCategoryData);
-			})
-			.catch((err) => {
-				console.log(err);
-				res.status(500).json(err);
-			});
-	});
-	// be sure to include its associated Products
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err));
+});
 
 router.post('/', (req, res) => {
-	// create a new category
-	Category.create({
-		category_name: req.body.category_name,
-	})
-		.then((dbPostData) => res.json(dbPostData))
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json(err);
-		});
-
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
-	// update a category by its `id` value
-  Category.update
-    (req.body, {
-      individualHooks: true,
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((dbCategoryData) => {
-        if (!dbCategoryData[0]) {
-          res.status(404).json({ message: 'No category found with this id' });
-          return;
-        }
-        res.json(dbCategoryData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
   })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
 
-  router.delete('/:id', (req, res) => {
-    //delete a category by it's id
-    Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((dbCategoryData) => {
-        if (!dbCategoryData) {
-          res.status(404).json({ message: 'No user category with this id' });
-          return;
-        }
-        res.json(dbCategoryData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
 
 module.exports = router;
